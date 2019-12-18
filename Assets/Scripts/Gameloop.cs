7e97b0084215112f34dee65f01f56c3e8cac0987
James Li <ryojamesli@gmail.com>
2019-12-18 17:30:35 +0100

    KinectView added
    KinectView, Gameloop added

---
+++ Assets/Scripts/Gameloop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameloop : MonoBehaviour
+{
+    [SerializeField] private GameObject m_Obstacle;
+    [SerializeField] private float m_SpawnTime;
+
+    private const float m_SpawnMax = 20f;
+
+    private IEnumerator Spawner()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(m_SpawnTime);
+            Instantiate(m_Obstacle, new Vector3(Random.Range(-m_SpawnMax, m_SpawnMax), .3f, 200), Quaternion.identity);
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Spawner());
+    }
+
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+
+    [SerializeField] private float m_Speed;
+    private Rigidbody m_Rigidbody;
+    private Vector3 m_Velocity;
+
+    private void OnDeath()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Velocity = Vector3.back;
+    }
+
+    private void Start()
+    {
+        m_Rigidbody.velocity = m_Velocity * m_Speed;
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_Rigidbody.transform.position.z < -18) OnDeath();
+    }
+
+}
