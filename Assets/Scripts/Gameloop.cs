e18965fbb0eeb1669ad296091b4245a5727cb869
James Li <ryojamesli@gmail.com>
2020-01-18 07:15:06 +0100

    SageModePoses added

--- Assets/Scripts/Gameloop.cs
+++ Assets/Scripts/Gameloop.cs
@@ -8,11 +8,9 @@ public class Gameloop : MonoBehaviour
     private readonly float []Lanes = new float [2] {-10f, 10f};
 
     [SerializeField] private GameObject m_Obstacle;
-    [SerializeField] private GameObject m_Player;
-    [SerializeField] private GameObject m_Ground;
     [SerializeField] private float m_SpawnTime;
+    [SerializeField] private float m_FloorSpeed = 5f;
 
-    private Material m_PlaneMaterial;
     private float m_PlayerVel;
     private const float m_SpawnMax = 20f;
 
@@ -27,16 +25,7 @@ public class Gameloop : MonoBehaviour
 
     private void Start()
     {
-        m_PlaneMaterial = m_Ground.GetComponent<Renderer>().material;
         StartCoroutine(Spawner());
-    }
-
-    private void FixedUpdate()
-    {
-        float currVel = m_Player.GetComponent<PlayerController>().Velocity;
-        Vector2 currPos = m_PlaneMaterial.GetTextureOffset("_BaseMap");
-        Vector2 newPos = new Vector2(0, (currPos.y - Time.deltaTime * 5 * currVel)%100);
-        m_PlaneMaterial.SetTextureOffset("_BaseMap", newPos);
     }
 
 }
---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    [SerializeField] private GameObject m_Lookat;
+
+    private void FixedUpdate()
+    {
+        transform.LookAt(m_Lookat.transform);
+    }
+}
