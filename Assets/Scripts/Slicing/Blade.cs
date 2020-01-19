37adc1ffcd8a3028450042b1565bc180c6544339
James Li <ryojamesli@gmail.com>
2020-01-19 17:10:30 +0100

    Cutting success

---
+++ Assets/Scripts/Slicing/Blade.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    [SerializeField] private GameObject trackedHand;
+    [SerializeField] private bool mouseDrawn;
+
+    //Blade is Publisher for Event OnLineDrawn
+    //delegate is a "pointer" to a function and specifies the listener function's signature
+    public delegate void LineDrawnHandler(Vector3 begin, Vector3 end, Vector3 depth);
+    public event LineDrawnHandler OnLineDrawn;
+
+    private const float minBladeSpeed = 1.4f;
+    private const float bladeDur = .2f;
+    private readonly Vector3 initPos = new Vector3(0, -20f, -10f);
+
+    private Camera cam;
+    private GameObject bladeTrail;
+    private DetectJoints detectJoints; 
+    private Transform handTf;
+    private Vector3 lastPosition;
+    private bool bladeActive = false;
+
+
+    private void Awake()
+    {
+        handTf = trackedHand.GetComponent<Transform>();
+        bladeTrail = transform.GetChild(0).gameObject;
+        detectJoints = GetComponent<DetectJoints>();
+
+        if (mouseDrawn)
+        {
+            detectJoints.enabled = false;
+        }
+    }
+
+    private void Start()
+    {
+        lastPosition = initPos;
+        bladeTrail.SetActive(false);
+        cam = GetComponent<DetectJoints>().CameraTrack;
+    }
+
+    private void Update()
+    {
+        float velocity = (handTf.position - lastPosition).magnitude * Time.deltaTime * 1000f;
+        if(!mouseDrawn && !bladeActive && lastPosition != initPos && velocity > minBladeSpeed)
+        {
+            bladeActive = true;
+            StartCoroutine(StartCut());
+        }
+        else if(mouseDrawn && Input.GetMouseButtonDown(0))
+        {
+            bladeActive = true;
+            StartCoroutine(StartCut());
+        }
+
+        lastPosition = handTf.position;
+
+        if (mouseDrawn)
+        {
+            handTf.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1f));
+        }
+    }
+
+    /// <summary>
+    /// Start´s Cutanimation / Trail
+    /// Ends when the velocity goes back => bladeActive == false;
+    /// A and B are the Start and End Point of the Cut;
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator StartCut()
+    {
+        Vector3 A = mouseDrawn ? Input.mousePosition : cam.WorldToScreenPoint(handTf.position);
+
+        //Activate Blade; Bladetime == bladeDur
+        bladeTrail.SetActive(true);
+        yield return new WaitForSeconds(bladeDur);
+        bladeTrail.SetActive(false);
+        bladeActive = false;
+
+
+        Vector3 B = mouseDrawn ? Input.mousePosition : cam.WorldToScreenPoint(transform.position);
+
+        //Invoke Event passing the Data to HandSlice
+        var start = cam.ScreenPointToRay(A);
+        var end = cam.ScreenPointToRay(B);
+
+        OnLineDrawn?.Invoke(start.GetPoint(cam.nearClipPlane),
+            end.GetPoint(cam.nearClipPlane),
+            end.direction.normalized);
+    }
+
+}
---
+++ Assets/Scripts/Slicing/Slice.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+
+public class Slice : MonoBehaviour
+{
+    [SerializeField] private Transform plane;
+    [SerializeField] private Blade bladeScript;
+    [SerializeField] private Material crossMaterial;
+    [SerializeField] private LayerMask layerMask;
+
+    private const float sliceRadius = 6f;
+    private void OnEnable()
+    {
+        bladeScript.OnLineDrawn += OnLineDrawn;
+    }
+    private void OnDisable()
+    {
+        bladeScript.OnLineDrawn -= OnLineDrawn;
+    }
+    private void SetPlane(Vector3 start, Vector3 end, Vector3 normalVec)
+    {
+        Quaternion rotate = Quaternion.FromToRotation(Vector3.up, normalVec);
+
+        plane.transform.localRotation = rotate;
+        plane.transform.position = (end + start) / 2;
+        plane.gameObject.SetActive(true);
+    }
+    private void OnLineDrawn(Vector3 A, Vector3 B, Vector3 dir)
+    {
+        Vector3 tangent = (B - A).normalized;
+        SetPlane(A, B, Vector3.Cross(dir, tangent));
+        InitSlice();
+    }
+    private void InitSlice()
+    {
+        Collider [] hits = Physics.OverlapBox(plane.position, new Vector3(sliceRadius, 0.1f, sliceRadius), 
+            plane.rotation, layerMask);
+
+        if (hits.Length <= 0) return;
+
+        foreach(Collider c in hits)
+        {
+            SlicedHull hull = SliceObject(c.gameObject, crossMaterial);
+            if(hull != null)
+            {
+                GameObject upper = hull.CreateUpperHull(c.gameObject, crossMaterial);
+                GameObject lower = hull.CreateLowerHull(c.gameObject, crossMaterial);
+                AddHullComp(upper);
+                AddHullComp(lower);
+                Destroy(c.gameObject);
+            }
+        }
+    }
+    private SlicedHull SliceObject(GameObject target, Material crossM)
+    {
+        if (target.GetComponent<MeshRenderer>() == null)
+            return null;
+
+        return target.Slice(plane.position, plane.up, crossM);
+    }
+    private void AddHullComp(GameObject target)
+    {
+        target.layer = 8;
+        Rigidbody rb = target.AddComponent<Rigidbody>();
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        rb.useGravity = false;
+        MeshCollider collider = target.AddComponent<MeshCollider>();
+        collider.convex = true;
+
+        rb.AddExplosionForce(30, target.transform.position, 20);
+        rb.velocity = Vector3.back;
+    }
+
+}
