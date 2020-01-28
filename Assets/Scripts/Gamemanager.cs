b77c27442e40000ea2295c780700a8d02581c9ba
James Li <ryojamesli@gmail.com>
2020-01-29 00:20:15 +0100

    CyperCity added
    Big visual asset update

--- Assets/Scripts/Gamemanager.cs
+++ Assets/Scripts/Gamemanager.cs
@@ -7,38 +7,19 @@ public class Gamemanager : MonoBehaviour
 
     static Gamemanager current;
     [SerializeField] private GameObject[] m_Obstacle;
+    [SerializeField] private Transform[] terrains;
+    [SerializeField] private PlayerController player;
+    [SerializeField] private GameObject hand;
     [SerializeField] private float maxSpawnTime;
-    [SerializeField] private GameObject Hand;
+    public bool spawn;
 
-    private float powerDrain = 2f;
-    private float bladeCharge = 2f;
+    private const float powerDrain = 2f;
+    private const float bladeCharge = 2f;
+    private const float groundSpeed = 20f;
 
-    public bool statLock = false;
     [HideInInspector] public float power;
     [HideInInspector] public float bladeTime;
 
-    private IEnumerator CrystalSpawner()
-    {
-        while (true)
-        {
-            yield return new WaitWhile(() => Hand.activeSelf);
-            yield return new WaitForSeconds(Random.Range(0f, maxSpawnTime));
-            Vector3 spawnPos = new Vector3(Random.Range(-3.3f, 4.4f), -4.7f, 100f);
-            Quaternion spawnRot = Quaternion.Euler(Random.Range(-30f, 30f), Random.Range(0, 360f), Random.Range(-30f, 30));
-            Instantiate(m_Obstacle[Random.Range(0,2)], spawnPos, spawnRot);
-        }
-    }
-    private IEnumerator MeteorSpawner()
-    {
-        while (true)
-        {
-            yield return new WaitWhile(() => Hand.activeSelf);
-            yield return new WaitForSeconds(Random.Range(0f, maxSpawnTime));
-            Vector3 spawnPos = new Vector3(Random.Range(-3.3f, 4.4f), Random.Range(-1.7f, -1), 100f);
-            Quaternion spawnRot = Quaternion.Euler(Random.Range(-30f, 30f), Random.Range(0, 360f), Random.Range(-30f, 30));
-            Instantiate(m_Obstacle[2], spawnPos, spawnRot);
-        }
-    }
     private void Awake()
     {
         if (current != null && current != this)
@@ -51,16 +32,46 @@ public class Gamemanager : MonoBehaviour
     }
     private void Start()
     {
-        StartCoroutine(CrystalSpawner());
-        StartCoroutine(MeteorSpawner());
+        if (spawn)
+        {
+            StartCoroutine(CrystalSpawner());
+            StartCoroutine(MeteorSpawner());
+        }
     }
     private void FixedUpdate()
     {
-        if (!Hand.activeSelf)
+        foreach (Transform t in terrains)
+        {
+            t.position += Vector3.back * Time.deltaTime * player.Velocity * groundSpeed;
+        }
+        ///If in RunMode
+        if (!hand.activeSelf)
         {
             power -= Time.deltaTime * powerDrain;
             bladeTime += Time.deltaTime * bladeCharge;
-            Debug.Log("Power: " + power + " BladeTime: " + bladeTime);
+            //Debug.Log("Power: " + power + " BladeTime: " + bladeTime);
+        }
+    }
+    private IEnumerator CrystalSpawner()
+    {
+        while (true)
+        {
+            yield return new WaitWhile(() => hand.activeSelf);
+            yield return new WaitForSeconds(Random.Range(0f, maxSpawnTime));
+            Vector3 spawnPos = new Vector3(Random.Range(-3.3f, 4.4f), -4.7f, 100f);
+            Quaternion spawnRot = Quaternion.Euler(Random.Range(-30f, 30f), Random.Range(0, 360f), Random.Range(-30f, 30));
+            Instantiate(m_Obstacle[Random.Range(0, 2)], spawnPos, spawnRot);
+        }
+    }
+    private IEnumerator MeteorSpawner()
+    {
+        while (true)
+        {
+            yield return new WaitWhile(() => hand.activeSelf);
+            yield return new WaitForSeconds(Random.Range(0f, maxSpawnTime));
+            Vector3 spawnPos = new Vector3(Random.Range(-3.3f, 4.4f), Random.Range(-1.7f, -1), 100f);
+            Quaternion spawnRot = Quaternion.Euler(Random.Range(-30f, 30f), Random.Range(0, 360f), Random.Range(-30f, 30));
+            Instantiate(m_Obstacle[2], spawnPos, spawnRot);
         }
     }
 }
---
+++ Assets/Scripts/GroundSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpawn : MonoBehaviour
+{
+    [SerializeField] private Transform forward;
+    [SerializeField] private float begin;
+    [SerializeField] private float offset;
+    
+    private void Update()
+    {
+        Vector3 currPos = transform.localPosition;
+        if(currPos.z <= begin)
+        {
+            transform.localPosition = new Vector3(currPos.x, currPos.y,
+                forward.localPosition.z + offset);
+        }
+    }
+
+}
